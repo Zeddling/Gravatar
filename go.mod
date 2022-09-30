445624f09696d39ba3c59122bcfc506bee36d769
zeddling <victorzeddys@gmail.com>
2022-09-30 14:49:28 +0300

    Connect to blockfrost

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/Zeddling/gravatar
 
 go 1.19
+
+require (
+	filippo.io/edwards25519 v1.0.0 // indirect
+	github.com/blockfrost/blockfrost-go v0.1.0 // indirect
+	github.com/echovl/cardano-go v0.1.14 // indirect
+	github.com/echovl/ed25519 v0.2.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/blockfrost/blockfrost-go"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load(".env")
+	log.Println("Project Id: ", os.Getenv("PROJECT_ID"))
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	client := blockfrost.NewAPIClient(
+		blockfrost.APIClientOptions{
+			ProjectID: os.Getenv("PROJECT_ID"),
+			Server:    os.Getenv("CARDANO_TESTNET"),
+		},
+	)
+	info, err := client.Info(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("API Info: ", info.Url)
+	log.Println("API Version: ", info.Version)
 }
